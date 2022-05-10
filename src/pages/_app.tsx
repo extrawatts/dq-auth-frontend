ba9318ac21537a473ed16ef2d4ce833d49af6e47
kenny <emrebadem_@hotmail.com>
2022-05-10 04:39:23 +0300

    feat: basic login/signup page with supertokens

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,33 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import ModalsProvider from 'src/components/ui/organisms/modals';
+import SuperTokens from 'supertokens-auth-react';
+import ThirdPartyPasswordless from 'supertokens-auth-react/recipe/thirdpartypasswordless';
+import Session from 'supertokens-auth-react/recipe/Session';
+
+if (typeof window !== 'undefined') {
+  console.log('hello', process.env.NEXT_PUBLIC_APP_NAME);
+  SuperTokens.init({
+    appInfo: {
+      appName: process.env.NEXT_PUBLIC_APP_NAME,
+      apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN,
+      websiteDomain: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN,
+    },
+    recipeList: [
+      ThirdPartyPasswordless.init({
+        signInUpFeature: {
+          providers: [
+            ThirdPartyPasswordless.Github.init(),
+            ThirdPartyPasswordless.Google.init(),
+            ThirdPartyPasswordless.Apple.init(),
+          ],
+        },
+        contactMethod: 'EMAIL',
+      }),
+      Session.init(),
+    ],
+  });
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
